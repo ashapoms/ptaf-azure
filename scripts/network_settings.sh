81d8eb6fe1949d7b7a14fbe93e18f8c270e49c09
Leonid Dyachkov <ldyachkov@ptsecurity.ru>
2017-01-19 15:08:43 +0300

    add fixed init.wsc script

--- scripts/network_settings.sh
+++ scripts/network_settings.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-echo -e $1 > /etc/hostname
-IP=$(ifconfig eth0 | grep 'inet addr' | xargs | awk -F '[: ]' '{{print $3}}')
 HOSTNAME=$1
+IP=$(ifconfig eth0 | grep 'inet addr' | xargs | awk -F '[: ]' '{{print $3}}')
 
+echo -e $HOSTNAME > /etc/hostname
 echo -e '127.0.0.1 localhost' > /etc/hosts
 echo -e $IP $HOSTNAME >> /etc/hosts
+/etc/init.d/hostname.sh
+
+rm /opt/waf/conf/wsc/config.sqlite3
