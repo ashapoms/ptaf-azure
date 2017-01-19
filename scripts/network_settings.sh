ff2e1e6fcd2687a5e0d2f6d13e12d576cd7133b7
Leonid Dyachkov <ldyachkov@ptsecurity.ru>
2017-01-19 12:48:35 +0300

    use vm name as hostname

--- scripts/network_settings.sh
+++ scripts/network_settings.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+echo -e $1 > /etc/hostname
 IP=$(ifconfig eth0 | grep 'inet addr' | xargs | awk -F '[: ]' '{{print $3}}')
-HOSTNAME=$(hostname)
+HOSTNAME=$1
 
 echo -e '127.0.0.1 localhost' > /etc/hosts
 echo -e $IP $HOSTNAME >> /etc/hosts
