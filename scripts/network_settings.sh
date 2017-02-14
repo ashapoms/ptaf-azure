dbbbf3263fc9199504318d31d3a12e11366ff572
Leonid Dyachkov <ldyachkov@ptsecurity.ru>
2017-02-14 16:59:45 +0300

    fix for new image

--- scripts/network_settings.sh
+++ scripts/network_settings.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+HOSTNAME=$1
 IP=$(ifconfig eth0 | grep 'inet addr' | xargs | awk -F '[: ]' '{{print $3}}')
-HOSTNAME=$(hostname)
 
+echo -e $HOSTNAME > /etc/hostname
 echo -e '127.0.0.1 localhost' > /etc/hosts
 echo -e $IP $HOSTNAME >> /etc/hosts
+/etc/init.d/hostname.sh
