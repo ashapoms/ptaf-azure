9f22ed6b2d167a019700d55b32c2de0f4dcaa5b0
Leonid Dyachkov <ldyachkov@ptsecurity.ru>
2017-02-15 20:39:42 +0300

    fix extension script to handle possible problems

--- scripts/network_settings.sh
+++ scripts/network_settings.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#delete azcesd if exists
+SEARCH=$(apt-cache search azure-security)
+if [ -n "$SEARCH" ]; then
+    apt-get purge -y azure-security
+fi
+
 HOSTNAME=$1
 IP=$(ifconfig eth0 | grep 'inet addr' | xargs | awk -F '[: ]' '{{print $3}}')
 
