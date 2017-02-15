05122aa34155551fcafd69a035e9786004d40a3a
Leonid Dyachkov <ldyachkov@ptsecurity.ru>
2017-02-15 22:04:30 +0300

    fix network settings script

--- scripts/network_settings.sh
+++ scripts/network_settings.sh
@@ -9,7 +9,24 @@ fi
 HOSTNAME=$1
 IP=$(ifconfig eth0 | grep 'inet addr' | xargs | awk -F '[: ]' '{{print $3}}')
 
-echo -e $HOSTNAME > /etc/hostname
-echo -e '127.0.0.1 localhost' > /etc/hosts
-echo -e $IP $HOSTNAME >> /etc/hosts
-/etc/init.d/hostname.sh
+wsc <<EOF
+output automation
+
+if alias lo 0
+if set lo:0 inet_address 192.168.40.40
+if set lo:0 inet_netmask 255.255.255.255
+
+host add 192.168.40.40 $HOSTNAME
+hostname $HOSTNAME
+
+if mode eth0 dhcp
+if mode eth1 dhcp
+
+if mark lo:0
+if mark eth0
+if mark eth1
+
+config commit
+config sync
+
+EOF
