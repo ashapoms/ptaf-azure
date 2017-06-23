00da50f6b76ae1b437d119bfd6cb77e5e272b693
Leonid Dyachkov <ldyachkov@ptsecurity.ru>
2017-06-23 16:42:57 +0300

    fix network_settings.sh

--- scripts/network_settings.sh
+++ scripts/network_settings.sh
@@ -6,10 +6,19 @@ if [ -n "$SEARCH" ]; then
     apt-get purge -y azure-security
 fi
 
+HASPLM_CONFIG=/etc/hasplm/hasplm.ini
 HOSTNAME=$1
 
-/usr/local/bin/wsc <<EOF
-output automation
+cat <<EOF >> "${HASPLM_CONFIG}" 
+[REMOTE]
+broadcastsearch = 1
+aggressive = 1
+serversearchinterval = 30
+serveraddr = ptaf-license.ptsecurity.com
+EOF
+
+
+/usr/local/bin/wsc -e <<EOF
 
 if alias lo 0
 if set lo:0 inet_address 192.168.40.40
@@ -21,9 +30,12 @@ hostname $HOSTNAME
 if mode eth0 dhcp
 if mode eth1 dhcp
 
-if mark lo:0
 if mark eth0
 if mark eth1
+if mark lo:0
+
+feature set azure_byol true
+integration_mode reverse_proxy
 
 config commit
 config sync
