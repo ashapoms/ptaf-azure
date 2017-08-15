eb0197cb8193de314da15b02011b07fee30b1dae
Leonid Dyachkov <ldyachkov@ptsecurity.ru>
2017-08-15 19:32:36 +0300

    use rest api for additional settings

--- scripts/network_settings.sh
+++ scripts/network_settings.sh
@@ -1,28 +1,11 @@
 #!/bin/bash
 
-HASPLM_CONFIG=/etc/hasplm/hasplm.ini
 HOSTNAME=$1
-MONGOD_CONFIG=/etc/mongod.conf
 
 #delete azcesd if exists
 if [ -n "$(apt-cache search azure-security)" ]; then
     apt-get purge -y azure-security
 fi
-
-
-sed -i "s/bind_ip = kickstat,127.0.0.1/bind_ip = ${HOSTNAME},127.0.0.1/g" "${MONGOD_CONFIG}"
-monit -I restart mongod
-
-
-cat <<EOF > "${HASPLM_CONFIG}" 
-[REMOTE]
-broadcastsearch = 1
-aggressive = 1
-serversearchinterval = 30
-serveraddr = ptaf-license.ptsecurity.com
-EOF
-
-service aksusbd restart
 
 /usr/local/bin/wsc -e <<EOF
 
@@ -37,11 +20,19 @@ if mark eth1
 if mark lo:0
 
 feature set azure_byol true
+integration_mode reverse_proxy
+user activate apic
 
 config commit
 config sync
 
 EOF
 
-echo 'db.gateways.update({"active": false}, {$set:{"active": true})' \
-    | mongo waf -u root -p $(wsc -c "password list") --authenticationDatabase admin
+/opt/waf/data/waf-tools/waf_rest_configure.py
+
+/usr/local/bin/wsc -e <<EOF
+
+user deactivate apic
+config sync
+
+EOF
