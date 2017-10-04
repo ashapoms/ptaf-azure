3f333254e8522b87b8fb49745f9214bb136df2dc
Leonid Dyachkov <ldyachkov@ptsecurity.ru>
2017-10-04 17:56:04 +0300

    fix elasticsearch errors

--- scripts/network_settings.sh
+++ scripts/network_settings.sh
@@ -8,6 +8,14 @@ if [ -n "$(apt-cache search azure-security)" ]; then
     apt-get purge -y azure-security
 fi
 
+service elasticsearch stop
+
+/usr/lib/jvm/default-java/bin/java -Xms1429560k -Xmx1429560k -Djava.awt.headless=true -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:CMSInitiatingOccupancyFraction=75 -XX:+UseCMSInitiatingOccupancyOnly -XX:+HeapDumpOnOutOfMemoryError -XX:+DisableExplicitGC -Dfile.encoding=UTF-8 -Delasticsearch -Des.pidfile=/var/run/elasticsearch/elasticsearch.pid -Des.path.home=/usr/share/elasticsearch -cp :/usr/share/elasticsearch/lib/elasticsearch-1.7.1.jar:/usr/share/elasticsearch/lib/*:/usr/share/elasticsearch/lib/sigar/* -Des.default.config=/etc/elasticsearch/elasticsearch.yml -Des.default.path.home=/usr/share/elasticsearch -Des.default.path.logs=/var/log/elasticsearch -Des.default.path.data=/var/lib/elasticsearch -Des.default.path.work=/tmp/elasticsearch -Des.default.path.conf=/etc/elasticsearch org.elasticsearch.bootstrap.Elasticsearch &
+PID=$!
+
+sleep 20
+kill -9 $PID
+
 /usr/local/bin/wsc -e <<EOF
 
 host add 127.0.1.1 $HOSTNAME
