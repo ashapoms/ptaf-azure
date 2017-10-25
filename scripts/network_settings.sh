89e70ba47d881da50583327b58143d6e348fdb55
Leonid Dyachkov <ldyachkov@ptsecurity.ru>
2017-10-25 20:37:33 +0300

    fix bugs with licenses

--- scripts/network_settings.sh
+++ scripts/network_settings.sh
@@ -11,6 +11,15 @@ fi
 rm /opt/waf/conf/wsc/snapshots/default.sqlite3 || echo 'wsc snapshot does not exist'
 rm /opt/waf/conf/wsc/config.sqlite3 || echo 'wsc snapshot does not exist'
 
+ sed -i "s/LICENSE_SERVER_REST_API.*/LICENSE_SERVER_REST_API = 'https://ptaf-license.ptsecurity.com/api/v3/g" \
+        /opt/waf/python/lib/python2.7/site-packages/ui/config.py
+
+
+sed -i "s/\/opt/waf/artifacts/.*/ /g" \
+        /etc/rc.local
+
+
+
 WSC_MGMT_INTERFACE=eth0 WSC_WAN_INTERFACE=eth0 WSC_LAN_INTERFACE=eth1 \
 /usr/local/bin/wsc -e <<EOF
 
@@ -24,7 +33,7 @@ if mark eth0
 if mark eth1
 if mark lo:0
 
-feature set azure_byol true
+
 integration_mode reverse_proxy
 
 config commit
